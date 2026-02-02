dca52d11e5e36b5d8902acdf63ad0e2e11a095af
Abraham Pérez Bautista <ajperezbau@protonmail.com>
2026-02-02 20:06:14 +0000

    Improve light theme icon

--- src/mswDevtools.vue
+++ src/mswDevtools.vue
@@ -85,13 +85,11 @@
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
+                <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
               </svg>
               <svg
                 v-else
@@ -100,13 +98,19 @@
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 3v1m0 16v1m9-9h-1M4 9H3m15.364 6.364l-.707.707M6.343 6.343l-.707.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
+                <circle cx="12" cy="12" r="4" />
+                <path d="M12 2v2" />
+                <path d="M12 20v2" />
+                <path d="m4.93 4.93 1.41 1.41" />
+                <path d="m17.66 17.66 1.41 1.41" />
+                <path d="M2 12h2" />
+                <path d="M20 12h2" />
+                <path d="m6.34 17.66-1.41 1.41" />
+                <path d="m19.07 4.93-1.41 1.41" />
               </svg>
             </button>
             <button
@@ -1447,8 +1451,8 @@ const filteredActivityLog = computed(() => {
 .clear-button:hover,
 .theme-toggle-button:hover {
   background-color: var(--bg-tertiary);
-  border-color: var(--text-tertiary);
-  color: var(--text-main);
+  border-color: var(--accent-color);
+  color: var(--accent-color);
 }
 
 .reload-button {
