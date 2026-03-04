a883aba9c128a385b6bfa831c745829718ec7f0f
Abraham Pérez Bautista <ajperezbau@protonmail.com>
2026-03-04 20:20:12 +0000

    Restore playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,6 +74,6 @@ export default defineConfig({
   webServer: {
     command: "pnpm dev",
     url: "http://localhost:5173",
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
   },
 });
