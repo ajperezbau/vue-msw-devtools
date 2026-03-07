9b916ca4a0ed4723ffa92bf57ed5225291d5fcae
Abraham Pérez Bautista <ajperezbau@protonmail.com>
2026-03-07 19:24:30 +0000

    chore: Add script to build the demo web

---
+++ vite.demo.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// Configuración exclusiva para compilar el index.html de prueba
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: "dist-demo", // Genera la web de prueba en esta carpeta
+    emptyOutDir: true, // Limpia la carpeta antes de cada build
+  },
+});
